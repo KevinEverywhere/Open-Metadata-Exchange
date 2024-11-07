f1257455a03acb52c427a000b7facc9450190ee8
Kevin Ready <kevin.ready@gmail.com>
2024-11-07 06:18:44 -0800

    Added redirect to NotFoundPage, still needs sidebar

--- fe2/src/pages/AdvancedSearch/ui/AdvancedSearch.tsx
+++ fe2/src/pages/AdvancedSearch/ui/AdvancedSearch.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable react/jsx-no-bind */
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import Select from 'react-dropdown-select';
 import ArrowIcon from '../../../shared/assets/icons/arrow-back.svg';
@@ -13,81 +13,90 @@ import { AppLink } from 'shared/ui/AppLink/AppLink';
 // import { useEffect, useState } from 'react';
 
 interface AdvancedSearchProps {
-  className?: string;
+	className?: string;
 }
 
+const endpoint = process.env.REACT_APP_API_URL;
+
 export function AdvancedSearch({ className }: AdvancedSearchProps) {
-  const [values, setValues] = useState([]);
+	const _options = [
+		{
+			value: '1',
+			label: 'most',
+		},
+		{
+			value: '2',
+			label: 'louis',
+		},
+		{
+			value: '3',
+			label: 'viva',
+		},
+	];
+	const [values, setValues] = useState([]);
+	const [options, setOptions] = useState([_options]);
 
-    const options = [
-        {
-            value: '1',
-            label: 'most',
-        },
-        {
-            value: '2',
-            label: 'louis',
-        },
-        {
-            value: '3',
-            label: 'viva',
-        }];
-    // useEffect(() => {
-    //   fetch('/api/imls/v2/collections/browse')
-    //     .then(res => res.json())
-    //     .then(data => {
-    //       console.log(data.collections.filters[0].items)
-    //       setOptions(data.collections.filters[0].items)
-    //     }
-    //     )
-    // }, []);
+	useEffect(() => {
+		fetch(`${endpoint}/api/imls/v2/collections/browse`)
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data.collections.filters[0].items);
+				setOptions(data.collections.filters[0].items);
+			});
+	}, []);
 
-  return (
-    <div className={classNames(cls.Advanced, {}, [className])}>
-      <ArrowIcon className={cls.back_link_img} />
-      <AppLink to="/imls/explore-oer-exchange" aria-label="Go Back to Explore OER Exchange" className={cls.back_link} text='Back to Explore OER Exchange' />
-      <h1 className={cls.advanced_title}>Advanced Search</h1>
-      <div className="">
-        <div className="">
-          <h2 className={cls.advanced_subtitle}>Filters</h2>
-          <form>
-            <ul>
-              <li className={cls.advanced_filter_item}>
-                <p className={cls.advanced_filter_item_title}>Microsite Curators</p>
-                <div className={cls.include}>
-                  <p>Include:</p>
-                  <Select
-                    options={options}
-                    placeholder="Select"
-                    searchable={false}
-                    clearable
-                    multi
-                    onChange={(values) => setValues(values)}
-                    values={[]}
-
-                  />
-                </div>
-                <div className={cls.exclude}>
-                  <p>Exclude:</p>
-                  <Select
-                    options={options}
-                    placeholder="Select"
-                    searchable={false}
-                    clearable
-                    multi
-                    onChange={(values) => setValues(values)}
-                    values={[]}
-                  />
-                </div>
-              </li>
-            </ul>
-          </form>
-        </div>
-        <div className={cls.btns_wrapper}>
-          <Button title="Search" />
-          <Button title="Reset" />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={classNames(cls.Advanced, {}, [className])}>
+			<ArrowIcon className={cls.back_link_img} />
+			<AppLink
+				to="/imls/explore-oer-exchange"
+				aria-label="Go Back to Explore OER Exchange"
+				className={cls.back_link}
+				text="Back to Explore OER Exchange"
+			/>
+			<h1 className={cls.advanced_title}>Advanced Search</h1>
+			<div className="">
+				<div className="">
+					<h2 className={cls.advanced_subtitle}>Filters</h2>
+					<form>
+						<ul>
+							<li className={cls.advanced_filter_item}>
+								<p className={cls.advanced_filter_item_title}>
+									Microsite Curators
+								</p>
+								<div className={cls.include}>
+									<p>Include:</p>
+									<Select
+										options={options}
+										placeholder="Select"
+										searchable={false}
+										clearable
+										multi
+										onChange={(values) => setValues(values)}
+										values={[]}
+									/>
+								</div>
+								<div className={cls.exclude}>
+									<p>Exclude:</p>
+									<Select
+										options={options}
+										placeholder="Select"
+										searchable={false}
+										clearable
+										multi
+										onChange={(values) => setValues(values)}
+										values={[]}
+									/>
+								</div>
+							</li>
+						</ul>
+					</form>
+				</div>
+				<div className={cls.btns_wrapper}>
+					<Button title="Search" />
+					<Button title="Reset" />
+				</div>
+			</div>
+		</div>
+	);
 }
--- fe2/src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ fe2/src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,14 +1,25 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './NotFoundPage.module.scss';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 interface NotFoundPageProps {
-  className?: string;
+	className?: string;
 }
 
-export const NotFoundPage = ({ className }: NotFoundPageProps) => (
-  <div className={classNames(cls.NotFoundPage, {}, [className])}>
-    Page not found
-  </div>
-);
+export const NotFoundPage = ({ className }: NotFoundPageProps) => {
+	const navigate = useNavigate();
+	useEffect(() => {
+		setTimeout(() => {
+			navigate('/imls', { replace: true });
+		}, 5000);
+	}, []);
+
+	return (
+		<div className={classNames(cls.NotFoundPage, {}, [className])}>
+			<h1>Redirecting . . .</h1>
+		</div>
+	);
+};
 
 export default NotFoundPage;
